ee5041c99493fd992d81d0033c116a68eb080cf0
Frekidus Ketemaw <frekidus29@gmail.com>
2022-03-16 23:29:02 +0300

    four

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,15 @@
  **/
 int main(void)
 {
-	char frekidus[] = "_putchar";
-	int c;
+	char frekidus[] = "_putchar" ;
+	int c ;
 
-	for (c=0;c<8;c++)
+	for (c = 0; c < 8; c++)
 	{
-		_putchar(frekidus[c]);
+		_putchar(frekidus[c]) ;
 		
 	}
-	_putchar('\n');
+	_putchar('\n') ;
 
-	return (0);
+	return (0) ;
 }
